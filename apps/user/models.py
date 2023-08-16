6ce447c60a27ea41ddc45ae44cbbeeb7e4c791f9
ali.rasoulinejad <ali.rasoulinejad75@gmail.com>
2023-08-16 20:17:11 +0330

    create a model to store change data requests

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,12 @@ from django_prometheus.models import ExportModelOperationsMixin
 
 from apps.common.fields import JSONSchemaField
 from apps.common.models import BaseModel
-from apps.user.enums import PersonTypeEnum, StatusEnum
+from apps.user.enums import (
+    PersonTypeEnum,
+    StatusEnum,
+    RequestStatusEnum,
+    AuthorizeRequestEnum,
+)
 from apps.user.schemas.legal_data import legal_data_schema
 from apps.utils.validators import cellphone_validator
 
@@ -52,3 +57,18 @@ class User(ExportModelOperationsMixin("user"), BaseModel):
     @property
     def is_authenticated(self):
         return True
+
+
+class AuthorizeRequest(ExportModelOperationsMixin("authorize_request"), BaseModel):
+    request_type = models.PositiveSmallIntegerField(choices=AuthorizeRequestEnum.choices)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data = models.JSONField("data", default=dict)
+    status = models.PositiveSmallIntegerField(choices=RequestStatusEnum.choices, default=RequestStatusEnum.CREATED)
+
+    class Meta:
+        db_table = "authorize_requests"
+        verbose_name = "احراز هویت"
+        verbose_name_plural = "احراز هویت‌ها"
+
+    def __str__(self):
+        return f"{self.request_type} for {self.user_id}"
